c55f1b708970203bf09aa00294314b5a4c845372
Muhammad Awais Naveed <awaisn872@gmail.com>
2026-07-01 18:08:26 +0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,88 @@
+"""
+marks=[90,94.5,87.75]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+"""
+
+# list can store multiple types of data
+"""
+stda=["Awais",20,5.10]
+print(stda[0])
+print(stda[1])
+print(stda[2])
+"""
+
+# list can be changeable (muteable)
+"""
+stda=["Awais",20,5.10]
+print(stda)
+stda[1]=19
+print(stda)
+"""
+
+# list slicing
+
+"""
+marks=[87,97,76,67,56]
+print(marks[1:4])
+print(marks[:4])
+print(marks[1:])
+print(marks[-3:-1])
+"""
+
+
+# list method
+
+# append (add one element at end)
+"""
+list=[1,2,3,4]
+list.append(5)
+print(list)
+"""
+
+# sort(sort in asscending order)
+"""
+list=[3,1,2,4]
+list.sort()
+print(list)
+ 
+ # sort in descending order
+list=[3,2,1,4]
+list.sort(reverse=True)
+print(list)
+
+"""
+
+# reverse
+"""
+list=['a','b','c','d']
+list.reverse()
+print(list)
+"""
+
+# insert
+"""
+list[1,3,2]
+list.insert(1,2)
+print(list)
+"""
+
+# remove element
+"""
+list=[1,2,3,4]
+list.remove(1)
+print(list)
+"""
+
+
+# pop(remove element at specific index)
+"""
+list=[1,2,3,4]
+list.pop(2)
+print(list)
+"""
+
